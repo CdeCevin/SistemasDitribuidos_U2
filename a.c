11a8bbd4c47a8d265e41b4b6f64f3f778ab81d9f
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-04-10 13:41:27 -0400

    hOLA

--- a.c
+++ a.c
@@ -1,8 +1,8 @@
 #include <stdio.h> 
 
 int main() {
-    int a = 2;
-    int b = 10;
+    int a = 15;
+    int b = 2;
     int c = a + b;
     printf("The sum of %d and %d is %d\n", a, b, c);
     return 0;
