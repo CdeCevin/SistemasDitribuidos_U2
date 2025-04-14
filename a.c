ecd71714aee6f654ca61877e4ed9b40ea582bf62
Domi <92610173+dt-24@users.noreply.github.com>
2025-04-14 17:52:57 -0400

    lectura

--- a.c
+++ a.c
@@ -1,6 +1,27 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#define DIM 20
 
 int main() {
+    int i,j,SIZE;
+    float **BD;
+    scanf("%d",&SIZE);
+
+    BD = (float **)malloc(sizeof(float *)*SIZE);
+    for(i=0;i<SIZE;i++)
+        BD[i] = (float *)malloc(sizeof(float)*DIM);
+
+    for(i=0;i<SIZE;i++)
+        for(j=0;j<DIM;j++)
+            scanf("%f",&(BD[i][j]));
+    
+    printf("%d\n",SIZE);
+    for(i=0;i<SIZE;i++)
+    {
+        for(j=0;j<DIM;j++)
+            printf("%.2f ",BD[i][j]);
+        printf("\n");
+    }
     printf("Hola tonota");
     int a = 15;
     int b = 2;
