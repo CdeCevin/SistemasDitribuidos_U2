e84820f74360f2e54a85bf6afcbbfd0804b8bc89
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-04-14 18:06:37 -0400

    comit

--- a.c
+++ a.c
@@ -25,11 +25,5 @@ int main() {
             printf("%.2f ",BD[i][j]);
         printf("\n");
     }
-    printf("Hola tonota");
-    int a = 15;
-    int b = 2;
-    int c = a + b;
-    printf("The sum of %d and %d is %d\n", a, b, c);
-    
     return 0;
 }
