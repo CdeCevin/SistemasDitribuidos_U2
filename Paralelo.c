bcddced7d9272df9b6812e23e959cdf5ed241f49
Domi <92610173+dt-24@users.noreply.github.com>
2025-04-19 01:03:45 -0400

    comentarios y free asignaciones_previas

--- Paralelo.c
+++ Paralelo.c
@@ -9,10 +9,12 @@
 #include <omp.h>
 #include <math.h>
 
+// Definicion de constantes
 #define DIM 20
 #define K 3
 #define T 8
 
+// Declaracion de funciones
 float calcular_distancia(float *fila, float *centroide);
 
 int main() {
@@ -24,6 +26,7 @@ int main() {
     omp_set_dynamic(0);
     omp_set_num_threads(T);
 
+    // Reserva de memoria dinamica para la base de datos y lectura
     BD = (float **)malloc(sizeof(float *) * SIZE);
     for (i = 0; i < SIZE; i++)
         BD[i] = (float *)malloc(sizeof(float) * DIM);
@@ -33,17 +36,20 @@ int main() {
             scanf("%f", &BD[i][j]);
 
     srand(time(NULL));
-
     double inicioT = omp_get_wtime();
+
+    // Reserva de memoria dinamica para K filas de los centroides con DIM elementos
     float **centroide = (float **)malloc(sizeof(float *) * K);
     for (i = 0; i < K; i++)
         centroide[i] = (float *)malloc(sizeof(float) * DIM);
 
+    // Arreglo auxiliar para reconocer los centroides ya escogidos, se inicializan todos en 0
     int usados[SIZE];
     for (i = 0; i < SIZE; i++)
         usados[i] = 0;
 
-    // Seleccionar K elementos aleatorios como centroides iniciales
+    // Seleccionar K elementos aleatorios como centroides iniciales entre 0 y SIZE-1
+    // El ciclo se repite hasta encontrar un indice que no haya sido utilizado
     for (i = 0; i < K; i++) {
         int idx;
         do {
@@ -56,35 +62,46 @@ int main() {
             centroide[i][j] = BD[idx][j];
     }
 
-
-
+    // Se reserva memoria dinamica para la matriz de asignaciones con la forma [indice cluster, indice vector]
     int **asignaciones = (int **)malloc(sizeof(int *) * SIZE);
     for (i = 0; i < SIZE; i++)
         asignaciones[i] = (int *)malloc(sizeof(int) * 2);
-
+    // Se reserva memoria dinamica para la matriz de asignaciones previas que permite detectar cambios entre iteraciones
     int *asignaciones_previas = (int *)malloc(sizeof(int) * SIZE);
     for (i = 0; i < SIZE; i++)
         asignaciones_previas[i] = -1;
 
+
     int cambio;
 
+    // Algoritmo principal K-means
+    // Se repite mientras existan cambios en las asignaciones vector-centroide
     do {
         cambio = 0;
 
+        // Copia las asignaciones de la iteracion a las asignaciones previas antes de hacer el siguiente calculo
         for (i = 0; i < SIZE; i++)
             asignaciones_previas[i] = asignaciones[i][0];
 
+        // Para realizar el trabajo en paralelo este se divide entre los hilos
         #pragma omp parallel
         {
             int id = omp_get_thread_num();
             int num_threads = omp_get_num_threads();
             int elementos_por_hilo = SIZE / num_threads;
             int inicio = elementos_por_hilo * id;
-            int fin = (id == num_threads - 1) ? SIZE : inicio + elementos_por_hilo;
-
+            int fin;
+            if(id == num_threads-1){
+                fin = SIZE;
+            }
+            else{
+                fin = inicio + elementos_por_hilo;
+            }
             int cambio_local = 0;
 
-            // Fase 1: Asignar elementos a centroides
+            // Fase 1: Asignar elementos de la base de datos a los centroides segun el calculo de su distancia euclidiana
+            // Se calcula la distancia respecto al primer centroide como base, luego esta se compara con las distancias calculadas
+            // para el resto de centroides y se asocia el indice del vector al indice del centroide con la distancia mas baja 
             for (int i = inicio; i < fin; i++) {
                 float minDist = calcular_distancia(BD[i], centroide[0]);
                 int minIdx = 1;
@@ -100,6 +117,7 @@ int main() {
                 asignaciones[i][0] = minIdx;
                 asignaciones[i][1] = i;
 
+                // Si existe un cambio respecto a la iteracion anterior se señala para continuar con el algoritmo
                 if (asignaciones[i][0] != asignaciones_previas[i])
                     cambio_local = 1;
             }
@@ -112,7 +130,7 @@ int main() {
             // Sincronizar antes de modificar centroides
             #pragma omp barrier
 
-            // Fase 2: Recalcular centroides
+            // Fase 2: Por cada centroide recalcular su valor a traves de los vectores asignados a este
             for (int c = 1; c <= K; c++) {
                 static float suma_global[K][DIM];
                 static int count_global[K];
@@ -132,14 +150,18 @@ int main() {
                 float suma_local[K][DIM] = {0};
                 int count_local[K] = {0};
 
+                // Se divide el trabajo entre los hilos, cada hilo acumula (dentro de su variable local) la suma 
+                // de los vectores asignados a cada centroide y se registra la cantidad de vectores asignados a este
                 for (int i = inicio; i < fin; i++) {
                     int c = asignaciones[i][0] - 1;
                     for (int j = 0; j < DIM; j++) {
-                        suma_local[c][j] += BD[i][j]; // Acumulación sin atomic
+                        suma_local[c][j] += BD[i][j]; 
                     }
                     count_local[c]++;
                 }
 
+                // Luego de que cada hilo calculara la suma local de datos se agrega esta a la
+                // suma global de cada centroide
                 #pragma omp critical
                 {
                     for (int c = 0; c < K; c++) {
@@ -149,9 +171,11 @@ int main() {
                         count_global[c] += count_local[c];
                     }
                 }
-
                 #pragma omp barrier
 
+                // Actualiza los centroides, primero asegura que el centroide se actualice solo si
+                // tiene vectores asignados a el, luego se actualiza el centroide dividiendo la suma
+                // acumulada de sus vectores por la cantidad total de vectores asignados 
                 #pragma omp single
                 {
                     if (count_global[c - 1] > 0) {
@@ -168,15 +192,18 @@ int main() {
     } while (cambio);
 
     double finT = omp_get_wtime();
+    printf("Tiempo de ejecucion: %.4f segundos\n", finT - inicioT);
 
+
+
+    // Liberacion de memoria
     for (i = 0; i < SIZE; i++)
         printf("%d\n", asignaciones[i][0]);
     
-    
-    printf("Tiempo de ejecucion: %.4f segundos\n", finT - inicioT);
     for (i = 0; i < SIZE; i++) {
         free(BD[i]);
         free(asignaciones[i]);
+        free(asignaciones_previas[i]);
     }
     free(BD);
     free(asignaciones);
@@ -189,6 +216,7 @@ int main() {
     return 0;
 }
 
+// Calcula la distancia euclidiana entre vectores con DIM dimensiones
 float calcular_distancia(float *fila, float *centroide) {
     float distancia = 0.0;
     for (int i = 0; i < DIM; i++) {
